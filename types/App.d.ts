9c10d15ee52ba0b81c166b385fc24b2113971ec2
Douglas Pérez <douglas.eduardo2000@gmail.com>
2023-04-12 10:41:10 -0400

    generate new build

--- types/App.d.ts
+++ types/App.d.ts
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.css';
 import './App.css';
 declare function App(): JSX.Element;
 export default App;
