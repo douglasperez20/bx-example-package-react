9f4dcfbc6791c3b49f66ffe599f146dee0446734
Douglas Pérez <douglas.eduardo2000@gmail.com>
2023-04-14 12:35:29 -0400

    fix css and add npmignore

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,38 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import atomico from '@atomico/vite';
 import svgr from 'vite-plugin-svgr';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), atomico(), svgr()],
+  plugins: [
+    react(),
+    atomico(),
+    svgr(),
+    cssInjectedByJsPlugin({
+      jsAssetsFilterFunction: function customJsAssetsfilterFunction(
+        outputChunk
+      ) {
+        return outputChunk.fileName == 'App.js';
+      },
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `${extType}/[name]-[hash][extname]`;
+        },
+
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
 });
